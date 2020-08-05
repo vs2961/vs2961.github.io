e64203e395a780e3e0f6e0ee2873ae5be4fd9766
Victor <siuleunging@gmail.com>
2020-08-05 16:43:17 -0400

    Updates

---
+++ precache-manifest.9f3eec74bfdfa0c86b17cf5fba8443eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1758e5c32bf5b0da9973d1f1d4340d50",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "93a36d22a01549139972",
+    "url": "/react-gh-pages/static/css/main.2b0a18c6.chunk.css"
+  },
+  {
+    "revision": "ca0ed17baf81815b1a2e",
+    "url": "/react-gh-pages/static/js/2.c27270c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.c27270c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93a36d22a01549139972",
+    "url": "/react-gh-pages/static/js/main.fc663a43.chunk.js"
+  },
+  {
+    "revision": "d77e0696d9db33b2b87f",
+    "url": "/react-gh-pages/static/js/runtime-main.3a2d02c5.js"
+  }
+]);
